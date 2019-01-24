c1023bdc897850d41939a25f5b30bb31e0830d69
Rodrigo Souza <rsouza01@gmail.com>
2019-01-23 22:20:31 -0200

    Some adjustments regarding react-dev-tools

--- run-mongo.sh
+++ run-mongo.sh
@@ -1,1 +1,4 @@
-docker run -d -p 27017:27017 -p 28017:28017 -e AUTH=no tutum/mongodb
+# docker run -d -p 27017:27017 -p 28017:28017 -e AUTH=no tutum/mongodb
+
+
+docker run -d -p 27017:27017 -v ~/mongodata:/data/db -e AUTH=no mongo
